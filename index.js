5defbad07815947d10fdb3f19e666814d9218bbb
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-05-26 02:06:54 -0400

    Index y CSS funcionando con carpetas públicas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import router from './route/router.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('/assets/css'))
+app.use(express.static('assets'))
 app.use('/', router);
 
 
