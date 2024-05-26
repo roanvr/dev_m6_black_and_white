edb610b9c3b2793489595d0cb04b3347eefe56be
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-05-26 02:47:56 -0400

    creación de ruta cargar + imagen modificada

--- route/router.js
+++ route/router.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import Jimp from 'jimp';
+import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 const __dirname = import.meta.dirname;
 import path from 'path';
@@ -6,5 +8,16 @@ import path from 'path';
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'))
 });
+
+router.get('cargar', async(req,res) => {
+    const { img } = req.query;
+    const imagenGuardada = `imagen${uuidv4().slice(0,4)}.jpg`
+    const imgJimp = await Jimp.read(img);
+    await imgJimp
+    .resize(350, AUTO)
+    .sepia()
+    .writeAsync(`assets/img/imagen${uuidv4().slice(0,4)}.jpg`);
+    res.sendFile(path.join(__dirname, `../assets/img/${imagenGuardada}`))
+})
 
 export default router
